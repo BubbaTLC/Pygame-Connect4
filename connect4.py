0b334ee1ef9fc0d4e09097e72e3c45c16ae915da
Bubba <chabot.tristan@gmail.com>
2020-11-08 16:02:20 -0600

    Finished Part 1 basic structure and game loop. --BC

---
+++ connect4.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+ROW_COUNT = 6
+COL_COUNT = 7
+
+
+def create_board():
+    board = np.zeros((ROW_COUNT, COL_COUNT))
+    return board
+
+
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+
+
+def is_valid_location(board, col):
+    return board[ROW_COUNT - 1][col] == 0
+
+
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+
+
+def print_board(board):
+    print(np.flip(board, 0))
+
+
+board = create_board()
+print_board(board)
+gameOver = False
+turn = 0
+
+while not gameOver:
+    # Ask for player 1 input
+    if turn == 0:
+        col = int(input("Player 1 Make your Selection (0-6):"))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 1)
+
+    # ask for player 2 input
+    else:
+        col = int(input("Player 2 Make your Selection (0-6):"))
+
+        if is_valid_location(board, col):
+            row = get_next_open_row(board, col)
+            drop_piece(board, row, col, 2)
+
+    print_board(board)
+
+    turn += 1
+    turn = turn % 2
